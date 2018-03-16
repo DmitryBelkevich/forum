04d6cda5a725151f1f5b48170cd552b88de83c77
hard <harleydav@mail.ru>
2018-03-16 17:01:21 +0300

    webapp // refactoring

--- webapp/src/main/java/com/hard/controllers/MainController.java
+++ webapp/src/main/java/com/hard/controllers/MainController.java
@@ -1,10 +1,7 @@
 package com.hard.controllers;
 
-import com.hard.services.MessageService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -12,15 +9,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class MainController {
     @GetMapping(value = "")
     public String main() {
-        return "main";
-    }
-
-    @Autowired
-    MessageService messageService;
-
-    @GetMapping(value = "/{id}")
-    public String delete(@PathVariable("id") long id) {
-        messageService.delete(id);
         return "main";
     }
 }
