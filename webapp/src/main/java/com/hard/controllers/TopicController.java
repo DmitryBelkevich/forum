d6de04a1933f5a43417dfc2b267f20b37bf68a4e
hard <harleydav@mail.ru>
2018-03-16 20:36:34 +0300

    webapp // topics_list_table fixed

--- webapp/src/main/java/com/hard/controllers/TopicController.java
+++ webapp/src/main/java/com/hard/controllers/TopicController.java
@@ -1,12 +1,21 @@
 package com.hard.controllers;
 
+import com.hard.models.Message;
+import com.hard.models.Topic;
+import com.hard.services.TopicService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.Collection;
+
 @Controller
 @RequestMapping("/topics")
 public class TopicController {
+    @Autowired
+    private TopicService topicService;
+
     @GetMapping(value = "")
     public ModelAndView getAll() {
         ModelAndView modelAndView = new ModelAndView("topics/topics_list");
@@ -17,6 +26,12 @@ public class TopicController {
     @GetMapping(value = "/{id}")
     public ModelAndView getById(@PathVariable long id) {
         ModelAndView modelAndView = new ModelAndView("topics/topic");
+
+        Topic topic = topicService.getById(id);
+
+        Collection<Message> messages = topic.getMessages();
+
+        modelAndView.addObject("messages", messages);
 
         return modelAndView;
     }
