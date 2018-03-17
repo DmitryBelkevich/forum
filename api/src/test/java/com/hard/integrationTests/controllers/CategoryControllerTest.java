49b5698a041ce27cb71b25ae237fd43c0751e297
hard <harleydav@mail.ru>
2018-03-17 17:49:53 +0300

    api // CategoryControllerTest getAll_shouldReturnNoContentStatus

---
+++ api/src/test/java/com/hard/integrationTests/controllers/CategoryControllerTest.java
@@ -0,0 +1,49 @@
+package com.hard.integrationTests.controllers;
+
+import com.hard.config.AppConfig;
+import com.hard.config.MvcConfig;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(
+        classes = {
+                AppConfig.class,
+                MvcConfig.class,
+        }
+)
+@WebAppConfiguration
+public class CategoryControllerTest {
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    private MockMvc mockMvc;
+
+    @Before
+    public void init() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+    }
+
+    @Test
+    public void getAll_shouldReturnNoContentStatus() throws Exception {
+        mockMvc.perform(
+                get("/api/categories")
+        )
+                .andExpect(status().isNoContent())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8_VALUE))
+        ;
+    }
+}
